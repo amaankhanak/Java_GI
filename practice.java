e3515d3b53558b7cab552896d845f8644769da0d
amaankhanakz <sameeramaan2014@gmail.com>
2023-08-18 11:08:12 +0530

    basics

---
+++ practice.java
@@ -0,0 +1,10 @@
+public class practice {
+  public static void main(String[] args) {
+    int a = 4;
+    int b = 7;
+    int c = 6;
+
+    int sum = a + b + c;
+    System.out.println(sum);
+  }
+}
